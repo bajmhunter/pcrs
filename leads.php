301ec9d218d27832f46fad72f9c4bdaa159356c8
fiokonkwo <fiokonkwo@gmail.com>
2010-04-21 10:36:40 -0500

    Tweaked auth.php, customers.php, home.php Added mysql_to date function in _php Fixed wrong access level in lead.php

--- leads.php
+++ leads.php
@@ -1,1 +1,1 @@
-<?php include('includes/_.php'); check_auth(); $_SESSION['view'] = 'Leads';  //only accessible to sales if ( $_SESSION['access_level'] != 2) {	die('<h1>Unauthorized</h1>'); } get_header();  ?><h1>Leads</h1><?php get_footer(); ?>
+<?php include('includes/_.php'); check_auth(); $_SESSION['view'] = 'Leads';  //only accessible to sales if ( $_SESSION['access_level'] != 3) {	die('<h1>Unauthorized</h1>'); } get_header();  ?><h1>Leads</h1><?php get_footer(); ?>
